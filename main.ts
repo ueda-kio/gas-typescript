8554c32a62d9a67e1195501aa70602feca2f5cfe
k-ueda <k_ueda@waku-2.com>
2023-11-22 10:58:34 +0900

    スタンプを指定可能とする

--- main.ts
+++ main.ts
@@ -4,21 +4,44 @@ if (SLACK_API_TOKEN === null) {
 }
 
 function doPost(e: GoogleAppsScript.Events.DoPost) {
-  const threadLink = e.parameter.text;
-  const threadData = getThreadData(threadLink);
+  const paramText = e.parameter.text;
+  if (!paramText) {
+    return ContentService.createTextOutput('チェックしたいスレッドのURLを引数で渡してください！:open_hands:');
+  }
+
+  const { threadUrl, argStamp } = extractUrlAndStampFromArgument(paramText);
+  if (argStamp === false) {
+    return ContentService.createTextOutput('第1引数にスレッドのurl、第2引数にスタンプを指定してください！スタンプは指定しなくても構いません！:open_hands:');
+  }
+
+  const threadData = getThreadData(threadUrl);
   if (!threadData) {
     return ContentService.createTextOutput('Error: スレッドデータの取得に失敗しました。:pien-2:');
   }
 
-  const { channelId, ts } = threadData;
-  const reactedUserIds = getReactedUserIds(channelId, ts);
+  const reactedUserIds = getReactedUserIds(threadData, argStamp);
   if (!reactedUserIds) return ContentService.createTextOutput('Error: スタンプを押したユーザー一覧の取得に失敗しました。:pien-2:');
 
-  const usersList = getUserListOfReactedOrNot(channelId, reactedUserIds);
+  const usersList = getUserListOfReactedOrNot(threadData.channelId, reactedUserIds);
   if (!usersList) return ContentService.createTextOutput('Error: ユーザー一覧の取得に失敗しました。:pien-2:');
 
   const { reactedUsers, nonReactedUsers } = usersList;
-  return ContentService.createTextOutput(`【スタンプつけた人】\n${returnLogText(reactedUsers)}\n\n【スタンプつけてない人】\n${returnLogText(nonReactedUsers)}`);
+  return ContentService.createTextOutput(
+    `【${argStamp || 'スタンプ'}つけた人】\n${returnLogText(reactedUsers)}\n\n【${argStamp || 'スタンプ'}つけてない人】\n${returnLogText(nonReactedUsers)}`
+  );
+}
+
+function extractUrlAndStampFromArgument(parameter: string) {
+  const spaceRegExp = /\s+|　+/;
+  if (!spaceRegExp.test(parameter)) return { threadUrl: parameter, argStamp: '' };
+
+  const splitParameter = parameter.split(spaceRegExp);
+  const threadUrl = splitParameter[0];
+  const argStamp = splitParameter[1];
+  const stampRegExp = /^:(.+):$/;
+  if (!stampRegExp.test(argStamp)) return { threadUrl, argStamp: false as const };
+
+  return { threadUrl, argStamp };
 }
 
 function getThreadData(threadLink: string) {
@@ -41,7 +64,7 @@ function getThreadData(threadLink: string) {
   }
 }
 
-function getReactedUserIds(channelId: string, ts: string) {
+function getReactedUserIds({ channelId, ts }: { channelId: string; ts: string }, argStamp: string) {
   try {
     const replies = UrlFetchApp.fetch('https://slack.com/api/conversations.replies', {
       headers: { Authorization: `Bearer ${SLACK_API_TOKEN}` },
@@ -52,13 +75,23 @@ function getReactedUserIds(channelId: string, ts: string) {
     });
 
     const repliesBody = JSON.parse(replies.getContentText());
-    const reactions = repliesBody.messages[0].reactions;
-    if (typeof reactions === 'undefined') return []; // no reacted users.
+    const reactions: {
+      name: string;
+      users: string[];
+      count: string;
+    }[] = repliesBody.messages[0].reactions;
+    if (typeof reactions === 'undefined') return []; // スタンプを押したユーザーがいない場合.
 
-    const _reactionUserIds = (reactions as { users: string[] }[]).flatMap((reaction) => reaction.users);
-    const reactionUserIds = Array.from(new Set(_reactionUserIds)); // Remove duplicates.
+    // 引数にスタンプを指定したかどうか.
+    if (argStamp) {
+      const reactionUserIds = reactions.find((reaction) => reaction.name === argStamp.replaceAll(':', ''))?.users ?? [];
+      return reactionUserIds;
+    } else {
+      const _reactionUserIds = reactions.flatMap((reaction) => reaction.users);
+      const reactionUserIds = Array.from(new Set(_reactionUserIds)); // 重複削除（いずれか1つ以上のスタンプを押しているか）
 
-    return reactionUserIds;
+      return reactionUserIds;
+    }
   } catch (e) {
     Logger.log(e);
     return false;
